4956553dc4548ebcf35ee51ef3efb606ccde9f3d
MD. SHOJOL ISLAM SHOJIB <sh0j0l3875891@gmail.com>
2024-12-08 15:53:39 +0600

    finish initial setup

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import "../global.css";
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MainLayout = () => {
+  return (
+    <View>
+      <Text className="text-white bg-red-500">MainLayout</Text>
+    </View>
+  )
+}
+
+export default MainLayout;
